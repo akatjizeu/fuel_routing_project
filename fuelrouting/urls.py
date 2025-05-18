b95317b46be14d3ecc9331f9a8d012d0e82923f1
Data Scientist <avikbw@gmail.com>
2025-05-18 18:02:21 +0200

    LAst commit

--- fuelrouting/urls.py
+++ fuelrouting/urls.py
@@ -1,4 +1,5 @@
-"""fuelrouting URL Configuration
+"""
+fuelrouting URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
